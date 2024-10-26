bc3e609fbff9bc9f6653894a4e63cfc2a1679d53
Maksim <kodermax@gmail.com>
2024-10-26 14:03:03 +0300

    upi

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,22 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { showRoutes } from "hono/dev";
+import { logger } from "hono/logger";
+const app = new Hono();
 
-const app = new Hono()
+app.use("/ping", logger());
+app.get("/ping", (c) => c.json({ ping: "pong" }, 200));
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+const isDev = process.env.NODE_ENV === "development";
+const port = 8000;
+
+if (isDev) showRoutes(app, { verbose: true, colorize: true });
+
+console.log(`Starting server on port ${port}`);
+
+const server = { port, fetch: app.fetch };
+
+export default server;
